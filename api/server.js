4afbe78e53ade8e74885b735cde8cba976df5d96
Hillary Cheserek <cheserekhillary@gmail.com>
2024-08-13 08:48:50 +0300

    Creating a database

--- api/server.js
+++ api/server.js
@@ -19,19 +19,89 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) return console.log("Error Connecting to DB")
     console.log("Connected to DB: ", db.threadId);
+
+
+    // create a database
+    db.query(`CREATE DATABASE IF NOT EXISTS my_expense_tracker`, (err, result) => {
+        if (err) return console.log(log);
+
+        console.log("Database created successfully");
+
+        // select the database
+        db.changeUser({ database: 'my_expense_tracker' }, (err) => {
+            if (err) return console.log(err);
+            console.log("database changed to my_expense_tracker");
+            // create users table
+            const createUsersTable = `
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL
+            )
+            `;
+
+            db.query(createUsersTable, (err, result) => {
+                if (err) return console.log(err);
+
+                console.log('Table created successfully');
+            });
+        });
+
+    });
 });
 
-// create a database
+// user registration route
+app.post('/api/register', async (req, res) => {
+    try {
+        const users = `SELECT * FROM users WHERE email = ?`;
 
-db.query(`CREATE DATABASE IF NOT EXISTS my_expense_tracker`, (err, result) => {
-    if(err) return console.log(log);
+        db.query(users, [req.body.email], (err, data) => {
+            if (data.length) return res.status(409).json("User already exists");
 
-    console.log("Database created successfully");
+            const salt = bcrypt.genSaltSync(10);
+            const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+            const newUser = `INSERT INTO users(email, username, password) VALUES(?)`
+            value = [
+                req.body.email,
+                req.body.username,
+                hashedPassword
+            ]
+
+            // insert into the database
+            db.query(newUser, [value], (err, data) => {
+                if (err) return res.status(500).json("Something went wromg");
+
+                return res.status(200).json("User created successfully");
+            })
+        })
+
+    }
+    catch (err) {
+        res.status(500).json("Internal Server Error");
+    }
 });
 
-app.get('', function(req, res){
-    res.send("Hello Hillary, What are you learning today? Please learn express");
-});
+// user login
+app.post('/api/login', async (rq, res) => {
+    try {
+        const users = `SELECT * FROM users WHERE email = ?`;
+        db.query(users, [req.body.email], (err, data) => {
+        if(data.length === 0) return res.status(404).json("User not found");
+        // Check is password is valid
+
+        const isPasswordValid = bcrypt.compareSync(req.body.password,data[0].password)
+
+        if(!isPasswordValid) return res.status(400).json("Ivalid email or password");
+
+        res.status(200).json("Login Successful");
+
+        })
+    } catch (err) {
+        res.status(500).json("Internal Server Error");
+    }
+})
 
 app.listen(3000, () => {
     console.log("Server us running at port 3000");
